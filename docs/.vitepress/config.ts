189acdd1ca369525cf4b86bc8f5118c39cfb66d0
yayxs <leaderywl@163.com>
2022-10-14 23:21:53 +0800

    add vitepress

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,45 @@
+import { defineConfig } from 'vitepress'
+const TITLE = '视野中国'
+const DESC = '程序员视野看中国'
+const generateSideBar = () => {
+  return [
+    {
+      text: 'About',
+      collapsible: true,
+      items: [
+        { text: 'What is china-city', link: '/guide/what-is-cc' },
+        { text: 'The author says', link: '/guide/the-author-says' },
+      ],
+    },
+    {
+      text: 'Update',
+      collapsible: true,
+      items: [
+        {
+          text: '0.x and 1.x',
+          link: '/guide/update-doc',
+        },
+      ],
+    },
+  ]
+}
+
+// /**
+//  * @type {import('vitepress').UserConfig}
+//  */
+// const config = {
+//   title: TITLE,
+//   description: DESC,
+//   themeConfig: {
+//     sidebar: generateSideBar(),
+//   },
+// }
+
+export default defineConfig({
+  title: TITLE,
+  description: DESC,
+  lastUpdated: true,
+  themeConfig: {
+    sidebar: generateSideBar(),
+  },
+})
---
+++ docs/guide/what-is-cc.md
@@ -0,0 +1,7 @@
+# What is China-city?
+
+`china-city` 最早是在`2022年`初萌生的`idea`。起初名为`vision-china` 意义为 “视野中国”。希望这个项目能在你的人生节点给到你帮助
+
+::: warning
+正在马不停蹄的开发中，
+:::
