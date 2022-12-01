39ec57eff1cb73031a6612fb613977c2b326b46b
yayxs <leaderywl@163.com>
2022-12-01 18:48:57 +0800

    添加一些国务院文件

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vitepress'
+import { withMermaid } from 'vitepress-plugin-mermaid'
 import { TITLE, DESC } from '../config'
 
 const generateSideBar = () => {
@@ -32,7 +33,7 @@ const generateSideBar = () => {
         { text: '首先了解中国所有的省份', link: '/guide/province' },
         { text: '其次知道省份下的所有城市', link: '/guide/province-city' },
         { text: '接着选择一个适合你的城市', link: '/guide/how-to-choose' },
-        { text: '怎么知道适不适合你', link: '/guide/priority' }
+        { text: '怎么知道适不适合你', link: '/guide/match' }
       ]
     },
 
@@ -53,26 +54,54 @@ const generateSideBar = () => {
         { text: '中国十四五与2035远景目标', link: '/guide/145-china' }
       ]
     },
+
     {
-      text: 'Quote / 引用',
-      collapsible: true,
-      items: [
-        { text: '二十大中的房地产', link: '/guide/fangjia-zz' },
-        { text: '重点专有名词解析', link: '/guide/proper-noun' }
-      ]
-    },
-    {
-      text: 'Other',
+      text: '城市专题',
       collapsible: true,
       items: [
         {
-          text: '买房日志',
+          text: '长沙',
+          link: '/cities/city-changsha'
+        },
+        {
+          text: '武汉',
+          link: '/cities/city-wuhan'
+        },
+        {
+          text: '西安',
+          link: '/cities/city-xian'
+        }
+      ]
+    },
+    {
+      text: '文献资料引用',
+      collapsible: true,
+      items: [{ text: '二十大中的房地产', link: '/guide/fangjia-zz' }]
+    },
+    {
+      text: '其他',
+      collapsible: true,
+      items: [
+        { text: '重点专有名词解析', link: '/guide/proper-noun' },
+        { text: '始终保持学习心态', link: '/guide/keep-learning' },
+        {
+          text: '作者买房日志与心得',
           link: '/guide/buying-a-house-log'
         }
       ]
     },
     {
-      text: 'Update',
+      text: '致谢',
+      collapsible: true,
+      items: [
+        {
+          text: '感谢技术',
+          link: '/thanks/technology'
+        }
+      ]
+    },
+    {
+      text: '更新迁移日志',
       collapsible: true,
       items: [
         {
@@ -95,11 +124,16 @@ const generateSideBar = () => {
 //   },
 // }
 
-export default defineConfig({
-  title: TITLE,
-  description: DESC,
-  lastUpdated: true,
-  themeConfig: {
-    sidebar: generateSideBar()
-  }
-})
+export default withMermaid(
+  defineConfig({
+    title: TITLE,
+    description: DESC,
+    lastUpdated: true,
+    themeConfig: {
+      sidebar: generateSideBar()
+    },
+    mermaid: {
+      // refer https://mermaid-js.github.io/mermaid/#/Setup for options
+    }
+  })
+)
--- docs/components/145/china.vue
+++ docs/components/145/china.vue
@@ -3,114 +3,92 @@ import type { S2DataConfig, S2Options } from '@antv/s2'
 import { SheetComponent } from '@antv/s2-vue'
 import { defineComponent, onMounted, reactive, ref, shallowRef } from 'vue'
 import '@antv/s2-vue/dist/style.min.css'
+import { info } from './china.data.js'
 const s2DataConfig = {
   fields: {
-    rows: ['province', 'city'],
-    columns: ['type'],
-    values: ['price']
+    rows: ['篇', '章'], // 行头
+    // columns: ['pian', 'zhang'],
+    values: ['核心内容']
   },
   data: [
     {
-      province: '浙江',
-      city: '杭州',
-      type: '笔',
-      price: '1'
+      篇: '开启全面建设社会主义现代化国家新征程',
+      章: '发展环境',
+      核心内容: `
+      我们要现代化国家：这个节点是第 2 个百年的首个第 5 年，生产总值突破 100 万亿元，经济实力、科技实力、综合国力
+      百年目标的第一个五年
+      粮食年产1.3万亿
+      金融风险处置
+      一带一路
+      教育公平
+      经济实力、综合国力、人民生活水平
+      战略机遇
+      经济全球化
+      `
     },
     {
-      province: '浙江',
-      city: '杭州',
-      type: '纸张',
-      price: '2'
+      篇: '开启全面建设社会主义现代化国家新征程',
+      章: '指导方针',
+      核心内容: `
+      十九大、十九届二中、三中、四中、五中
+      国内大循环为主体
+      `
     },
     {
-      province: '浙江',
-      city: '舟山',
-      type: '笔',
-      price: '17'
+      篇: '开启全面建设社会主义现代化国家新征程',
+      章: '主要目标',
+      核心内容: `
+    
+      `
+    },
+    // 第八篇 -----------------------
+    {
+      篇: '完善新型城镇化战略 提升城镇化发展质量',
+      章: '加快农业转移人口市民化',
+      核心内容: `
+        户籍制度改革
+      `
     },
     {
-      province: '浙江',
-      city: '舟山',
-      type: '纸张',
-      price: '6'
+      篇: '完善新型城镇化战略 提升城镇化发展质量',
+      章: '完善城镇化空间布局',
+      核心内容: `
+        社会主义新型城镇化
+        城市群、都市圈
+        大中小城市、小型城镇协调联动
+        城镇化发展 新型城镇化 城市群 都市圈 城市联动 户籍改革 **放宽落户政策** 常住人口 300 万一下落户限制取消 放宽 300 万至 500 万大城市落户条件 完善 
+        空间布局
+        两横三纵城镇化战略
+        城市群一体化发展
+        优化提升：京津冀、长三角、珠三角、成渝、长江中游
+        发展壮大：山东半岛、
+        现代化都市圈
+       
+      `
+    },
+    // 第九篇 -----------------------
+    {
+      篇: '优化区域经济布局 促进区域协调发展',
+      章: '第三十章　优化国土空间开发保护格局',
+      核心内容: `
+        保障国家重大发展战略落地实施
+        中心城市、城市群
+        京津冀、长三角、粤港澳大湾区 第一梯队
+        中西部提升城市群功能 重要区域
+      `
     },
     {
-      province: '吉林',
-      city: '长春',
-      type: '笔',
-      price: '8'
+      篇: '优化区域经济布局 促进区域协调发展',
+      章: '第三十一章　深入实施区域重大战略',
+      核心内容: `
+       雄安新区
+      `
     },
-    {
-      province: '吉林',
-      city: '白山',
-      type: '笔',
-      price: '12'
-    },
-    {
-      province: '吉林',
-      city: '长春',
-      type: '纸张',
-      price: '3'
-    },
-    {
-      province: '吉林',
-      city: '白山',
-      type: '纸张',
-      price: '25'
-    },
-    {
-      province: '浙江',
-      city: '杭州',
-      type: '笔',
-      cost: '0.5'
-    },
-    {
-      province: '浙江',
-      city: '杭州',
-      type: '纸张',
-      cost: '20'
-    },
-    {
-      province: '浙江',
-      city: '舟山',
-      type: '笔',
-      cost: '1.7'
-    },
-    {
-      province: '浙江',
-      city: '舟山',
-      type: '纸张',
-      cost: '0.12'
-    },
-    {
-      province: '吉林',
-      city: '长春',
-      type: '笔',
-      cost: '10'
-    },
-    {
-      province: '吉林',
-      city: '白山',
-      type: '笔',
-      cost: '9'
-    },
-    {
-      province: '吉林',
-      city: '长春',
-      type: '纸张',
-      cost: '3'
-    },
-    {
-      province: '吉林',
-      city: '白山',
-      type: '纸张',
-      cost: '1'
-    }
   ]
 }
 const s2Options = {
   width: 600,
-  height: 480
+  height: 500
 }
 const dataCfg = shallowRef(s2DataConfig)
 const options: S2Options = reactive(s2Options)
